ece06047d34c7d2486a0e623dfac0dc39af0c2cd
Ramesh <98897378+sonusheorann@users.noreply.github.com>
2023-08-25 16:21:47 +0530

    Update MyContract.sol

--- Functions and Errors/MyContract.sol
+++ Functions and Errors/MyContract.sol
@@ -4,26 +4,39 @@ pragma solidity ^0.8.0;
 //For this project, write a smart contract that implements the require(), assert() and revert() statements.
 
 
-contract MyContract {
+contract myContract_for_errorHandling {
     address private owner;
+    uint public balance;
 
     constructor() {
         owner = msg.sender;
     }
+    
+    //Using Modifier to check if contract caller is owner of the contract
+    modifier check_Owner ()  {
+        require(owner==msg.sender,"Only Owner can access the Contract"); 
+        _;
+    }
 
+    function add_amount(uint amount) public check_Owner {
+        //require statement to check we are adding amount greater than 0.
+        require(amount > 0,"Amount should be greater than zero");        
+        balance+=amount;
+    }
 
-    function withdraw(uint amount) public view {
-        require(amount > 0,"Amount should be greater than zero");         //require statement
-
-        uint balance = address(this).balance;
-        assert(balance >= amount);                                        //assert statement                 
-        
+    function withdraw_amount(uint amount) public check_Owner {
+        //assert statement to check if we have sufficient balance to withdraw
+        assert(balance >= amount);                                           
+        balance-=amount;
+    }                                                   
+    function check_balance(uint amount) public view check_Owner{
         if(amount > balance){
-        revert("Withdrawal failed because of insufficient balance");      //revert statement
+            //revert statement for showing insufficient balance
+            revert("Withdrawal failed because of insufficient balance");       
         }                                                              
     }
 
     function getContractBalance() public view returns (uint) {
-        return address(this).balance;
+       return balance;
     }
 }
